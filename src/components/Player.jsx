1a0230429069068b46aae1c2dea4d08e3381d931
Nityom Tikhe <nityomtikherr@gmail.com>
2024-01-20 23:00:38 +0530

    tic-tac-toe game using react

---
+++ src/components/Player.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useState } from 'react'
+
+function Player({initialName,symbol,isActive}) {
+
+const [playerName,setPlayerName] = useState(initialName);
+
+const [isEditing,setIsEditing ] = useState(false);
+
+function handleEditClick(){
+setIsEditing((editing)=>!editing);
+
+
+
+}
+
+function handleChange(event){
+  setPlayerName(event.target.value);
+
+}
+
+let editableplayerName = <span className='player-name'>{playerName}</span>
+// let btnCaption = "Edit";
+
+if(isEditing){
+    editableplayerName =<input type="text" 
+    required value={playerName} onChange={handleChange}/>
+    // btnCaption='Save';
+}
+
+
+  return (
+    <div>
+       <li className={isActive ? 'active':undefined}> 
+    <span className="player">
+ {editableplayerName}
+  <span className="player-name">{symbol}</span>
+  </span>
+  <button onClick={handleEditClick}>{isEditing ? 'Save':'Edit'}
+  </button>
+  </li>
+    </div>
+  )
+}
+
+export default Player
